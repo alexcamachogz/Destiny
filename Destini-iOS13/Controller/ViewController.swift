543cb11cd4efbe0f9b931b3a41055d1aa06dd3d1
Alex Camacho <camacho.ale94@gmai.com>
2022-06-04 12:21:11 -0500

    Create main functionality of the app

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    lazy var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateView()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userAnswer = sender.title(for: .normal)!
+        
+        storyBrain.nextStory(userChoice: userAnswer)
+        
+        updateView()
+    }
+    
+    func updateView() {
+        storyLabel.text = storyBrain.getTitleText()
+        
+        let choices = storyBrain.getChoicesText()
+        if choices[0] == "0" {
+            choice1Button.isHidden = true
+        } else {
+            choice1Button.isHidden = false
+            choice1Button.setTitle(choices[0], for: .normal)
+        }
+        choice2Button.setTitle(choices[1], for: .normal)
+    }
 }
 
